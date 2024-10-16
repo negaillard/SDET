8521154016f36051d8e8fb5ea500bc451dd8cd16
antoc0der <1@DESKTOP-K1L8ND3>
2024-10-16 20:50:34 +0400

    lection 2 task

---
+++ src/lection2/Exceptions/WrongPasswordException.java
@@ -0,0 +1,11 @@
+package lection2.Exceptions;
+
+public class WrongPasswordException extends Exception {
+    public WrongPasswordException() {
+        super();
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
